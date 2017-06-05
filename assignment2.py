d9baa26e5d3bce05a3ce9d0e05b1b286c5f99b91
Keano Porcaro <keano.porcaro@gmail.com>
2017-06-05 13:55:32 +0930

    Update assignment2.py

--- assignment2.py
+++ assignment2.py
@@ -9,6 +9,25 @@ from decisionTreeUtils import extract_decisiontree_rules
 # QUESTION 1
 #
 
+def closestFood(pos, food, walls):
+    fringe = [(pos[0], pos[1], 0)]
+    expanded = set()
+    while fringe:
+        pos_x, pos_y, dist = fringe.pop(0)
+        if (pos_x, pos_y) in expanded:
+            continue
+        expanded.add((pos_x, pos_y))
+        # if we find a food at this location then exit
+        if food[pos_x][pos_y]:
+            return dist
+        # otherwise spread out from the location to its neighbours
+        nbrs = Actions.getLegalNeighbors((pos_x, pos_y), walls)
+        for nbr_x, nbr_y in nbrs:
+            fringe.append((nbr_x, nbr_y, dist+1))
+    # no food found
+    return None
+
+
 def extract_action_features(gameState, action):
     """
     This function extracts features reflecting the effects of applying action to gameState.
@@ -23,6 +42,34 @@ def extract_action_features(gameState, action):
     features['score'] = successorState.getScore()  # keep this?
     "*** YOUR CODE HERE ***"
     util.raiseNotDefined()
+
+    # extract the grid of food and wall locations and get the ghost locations
+    food = gameState.getFood()
+    walls = gameState.getWalls()
+    ghosts = gameState.getGhostPositions()
+
+    features = util.Counter()
+
+    features["bias"] = 1.0
+
+    # compute the location of pacman after he takes the action
+    x, y = gameState.getPacmanPosition()
+    dx, dy = Actions.directionToVector(action)
+    next_x, next_y = int(x + dx), int(y + dy)
+
+    # count the number of ghosts 1-step away
+    features["#-of-ghosts-1-step-away"] = sum((next_x, next_y) in Actions.getLegalNeighbors(g, walls) for g in ghosts)
+
+    # if there is no danger of ghosts then add the food feature
+    if not features["#-of-ghosts-1-step-away"] and food[next_x][next_y]:
+        features["eats-food"] = 1.0
+
+    dist = closestFood((next_x, next_y), food, walls)
+    if dist is not None:
+        # make the distance a number less than one otherwise the update
+        # will diverge wildly
+        features["closest-food"] = float(dist) / (walls.width * walls.height)
+    features.divideAll(10.0)
     return features
 
 
