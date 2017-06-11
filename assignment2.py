1fcfdb19a5c95aee8f90690d8616f0c67a763721
Keano Porcaro <porkd002@mymail.unisa.edu.au>
2017-06-11 12:18:27 +0930

    Lots done, Q5-6 to go.

--- assignment2.py
+++ assignment2.py
@@ -4,7 +4,13 @@
 import util
 from game import Actions
 from samples import prepare_dataset
+from sklearn.neural_network import MLPClassifier
 from decisionTreeUtils import extract_decisiontree_rules
+from sklearn.metrics import classification_report, accuracy_score
+from sklearn.externals.six import StringIO
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import GridSearchCV
+from sklearn import tree
 
 #
 # QUESTION 1
@@ -70,7 +76,7 @@ def extract_action_features(gameState, action):
         # will diverge wildly
         features["closest-food"] = float(dist) / (walls.width * walls.height)
     features.divideAll(10.0)
-    print(features)
+    #print(features)
     return features
 
 
@@ -84,26 +90,63 @@ def do_agent(agent):
     #
     # QUESTION 2
     #
-
-    "*** YOUR CODE HERE ***"
+    clf = tree.DecisionTreeClassifier()
+    clf.fit(X, y) # training data
 
     #
     # QUESTION 3
     #
+    
+    # 110171506
 
-    "*** YOUR CODE HERE ***"
+    # TRAINING data
+    y_pred = clf.predict(X)
+    print('Correctly predicted on TRAINING set: {}, errors: {}'.format(sum(y==y_pred), sum(y!=y_pred)))
+    print(classification_report(y, y_pred))
+    print('Accuracy on TRAINING set: {:.2f}'.format(accuracy_score(y, y_pred)) , '\n')
+    print("Confusion Matrix:\n "," ".join(["{:3d}".format(d) for d in clf.classes_]),"<-- PREDICTED LABEL")
+    print(confusion_matrix(y,y_pred,labels=clf.classes_))
 
+
+    # TEST data
+    #y_pred = clf.predict(Xt)
+    print('Correctly predicted on TEST set: {}, errors: {}'.format(sum(yt==y_pred), sum(yt!=y_pred)))
+    print(classification_report(yt, y_pred))
+    print('Accuracy on TEST set: {:.2f}'.format(accuracy_score(yt, y_pred)))
+    print("Confusion Matrix:\n "," ".join(["{:3d}".format(d) for d in clf.classes_]),"<-- PREDICTED LABEL")
+    print(confusion_matrix(yt,y_pred,labels=clf.classes_), "\n")
+        
+    
     #
     # QUESTION 4
     #
 
-    "*** YOUR CODE HERE ***"
+    # Multi-Layer-Perceptron
+    mlp = MLPClassifier(max_iter=5000)
+    mlp.fit(X, y)
+    
+    # TRAINING data
+    y_pred = mlp.predict(X)
+    print('Correctly predicted on TRAINING set with MLP classifier: {}, errors: {}'.format(sum(y==y_pred), sum(y!=y_pred)))
+    print(classification_report(y, y_pred))
+    print('Accuracy on TRAINING set with MLP classifier: {:.2f}'.format(accuracy_score(y, y_pred)))
+    print("Confusion Matrix:\n "," ".join(["{:3d}".format(d) for d in mlp.classes_]),"<-- PREDICTED LABEL")
+    print(confusion_matrix(y,y_pred,labels=mlp.classes_))
+    
+    # TEST data
+    y_pred = mlp.predict(Xt)
+    print('Correctly predicted on TEST set: {}, errors: {}'.format(sum(yt==y_pred), sum(yt!=y_pred)))
+    print(classification_report(yt, y_pred))
+    print('Accuracy on TEST set: {:.2f}'.format(accuracy_score(yt, y_pred)))
+    print("Confusion Matrix:\n "," ".join(["{:3d}".format(d) for d in mlp.classes_]),"<-- PREDICTED LABEL")
+    print(confusion_matrix(yt,y_pred,labels=mlp.classes_), "\n")
 
     #
     # QUESTION 5
     #
 
-    "*** YOUR CODE HERE ***"
+    parameters = {'hidden_layer_sizes': np.arrange()}
+    
 
     #
     # QUESTION 6: see file classifierAgents.py
@@ -111,7 +154,7 @@ def do_agent(agent):
 
 
 if __name__ == '__main__':
-    for agent in ['food', 'leftturn', 'random', 'stop', 'suicide']: # pick agents you need to consider
+    for agent in ['leftturn', 'random', 'stop', 'suicide']: # pick agents you need to consider
         print("AGENT", agent, "\n" + "=" * 40, "\n")
         do_agent(agent)
         print("\n\n\n")
